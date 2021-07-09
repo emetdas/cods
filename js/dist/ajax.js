760f34e32060df1859072497448cab65ee902c23
emetdas <emet@b2bleadstaff.com>
2021-07-09 17:04:55 +0600

    some ajax update is done

---
+++ js/dist/ajax.js
@@ -0,0 +1,8 @@
+// http://youmightnotneedjquery.com/
+// https://blog.garstasio.com/you-dont-need-jquery/ajax/
+// https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX/Getting_Started
+// https://www.sitepoint.com/guide-vanilla-ajax-without-jquery/
+// setRequestHeader
+setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+setRequestHeader("X-Requested-With", "XMLHttpRequest");
+// setRequestHeader
