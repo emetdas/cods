1c569ed1fb4d510960a4f9d5d2f54fca28d35a38
emetdas <emet@b2bleadstaff.com>
2021-07-10 19:10:41 +0600

    new link add in ajax file

--- js/dist/ajax.js
+++ js/dist/ajax.js
@@ -2,6 +2,7 @@
 // https://blog.garstasio.com/you-dont-need-jquery/ajax/
 // https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX/Getting_Started
 // https://www.sitepoint.com/guide-vanilla-ajax-without-jquery/
+// https://xhr.spec.whatwg.org/
 // setRequestHeader
 setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 setRequestHeader("X-Requested-With", "XMLHttpRequest");
