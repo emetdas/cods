05f82e03bff024927081818b03134b97fb973bda
emetdas <emet@b2bleadstaff.com>
2021-07-27 10:30:30 +0600

    new update is done

--- js/dist/navbar.js
+++ js/dist/navbar.js
@@ -1,15 +1,17 @@
-function navbar(){
-    let nav_icon = document.querySelectorAll('[data-navIcon="nav_icon"]');
-    let nav_manue = document.querySelector('[data-navManue="nav_manue"]');
-    nav_icon.forEach(function(icon){
-        icon.addEventListener('click',()=>{ 
-            icon.classList.toggle('active');
-            nav_manue.classList.toggle('active');
-        })
+function navbar() {
+  let nav_icon = document.querySelectorAll('[data-navIcon="nav_icon"]');
+  let nav_manue = document.querySelector('[data-navManue="nav_manue"]');
+  nav_icon.forEach(function (icon) {
+    icon.addEventListener('click', () => {
+      icon.classList.toggle('active');
+      nav_manue.classList.toggle('active');
     });
-    let desk_low = window.matchMedia("(min-width: 992px)");
-    if (desk_low.matches) {
-        nav_manue.classList.remove('active');
-    }
+  });
 }
 navbar();
+// window.addEventListener('resize', () => {
+//   let desk_low = window.matchMedia('(min-width: 992px)');
+//   if (desk_low.matches) {
+//     nav_manue.classList.remove('active');
+//   }
+// });
